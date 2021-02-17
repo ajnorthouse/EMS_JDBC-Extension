73d2a85a2029575cbb47d7e94854454969aba3aa
zelda <raymundpalafox1@gmail.com>
2021-02-17 13:48:34 -0800

    Add Company, Dept mySQL Struct. files

---
+++ sql/tables/Department.sql
@@ -0,0 +1,11 @@
+CREATE table department
+(
+	dept_id INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+    dept_budget INT NOT NULL,
+    dept_name VARCHAR(50) NOT NULL,
+    dept_manager_name VARCHAR(50) NOT NULL,
+    dept_manager_num VARCHAR(20),
+    dept_manager_email VARCHAR(50) NOT NULL,
+    comp_id INT NOT NULL,
+    FOREIGN KEY (comp_id) references company(comp_id)
+);
