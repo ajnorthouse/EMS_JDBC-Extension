55bba4069ec78ced684d0fd76063abdae6252aa9
Raymund <raymundpalafox1@gmail.com>
2021-02-17 19:39:51 -0800

    Completed department table

---
+++ sql/tables/department.sql
@@ -0,0 +1,9 @@
+CREATE table department
+(
+	dept_id INT PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	dept_name VARCHAR(50) NOT NULL,
+    dept_phone_num VARCHAR(11),
+	dept_budget INT NOT NULL,
+    comp_id INT NOT NULL,
+    FOREIGN KEY (comp_id) references company(comp_id)
+);
