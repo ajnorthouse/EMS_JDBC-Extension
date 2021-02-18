d012ea316c1a338bb122686b7a0cc76efa62e3aa
Alex Donaldson <32708922+Bhff@users.noreply.github.com>
2021-02-18 15:43:53 -0500

    Completed EmployeeDAOClass

--- src/com/cognixia/jump/jdbc/ems_extension/team2/interfaces/EmployeeDAO.java
+++ src/com/cognixia/jump/jdbc/ems_extension/team2/interfaces/EmployeeDAO.java
@@ -6,7 +6,7 @@ import com.cognixia.jump.jdbc.ems_extension.team2.model.Employee;
 public interface EmployeeDAO {
 	public List<Employee> getAllEmployees();
 	public Employee getEmployee(int id);
-	public boolean createEmployee(Employee e);
-	public boolean deleteEmployee(Employee e);
-	public boolean updateEmployee(Employee e);
+	public boolean createEmployee(Employee em);
+	public boolean deleteEmployee(Employee em);
+	public boolean updateEmployee(Employee em);
 }
