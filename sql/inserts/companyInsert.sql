cdd08622cdf938f109a10715fddc2a7a3962cc2b
Raymund <raymundpalafox1@gmail.com>
2021-02-17 21:11:59 -0800

    Completed company inserts

---
+++ sql/inserts/companyInsert.sql
@@ -0,0 +1,8 @@
+INSERT INTO company(comp_id, comp_name, comp_budget, address_id) 
+			 VALUES(null, "Tesla", "5000000", 1);
+             
+INSERT INTO company(comp_id, comp_name, comp_budget, address_id)
+			 VALUES(null, "Amazon", "6500000", 2);
+             
+INSERT INTO company(comp_id, comp_name, comp_budget, address_id)
+			 VALUES(null, "Apple", "6000000", 3);
