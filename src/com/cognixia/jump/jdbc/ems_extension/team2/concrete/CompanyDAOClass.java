0fe738c327893017010ea4ff4b82da119f262b80
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-02-18 11:19:04 -0500

    Add PreparedStatement example to be deleted later.

--- src/com/cognixia/jump/jdbc/ems_extension/team2/concrete/CompanyDAOClass.java
+++ src/com/cognixia/jump/jdbc/ems_extension/team2/concrete/CompanyDAOClass.java
@@ -1,6 +1,9 @@
 package com.cognixia.jump.jdbc.ems_extension.team2.concrete;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.cognixia.jump.jdbc.ems_extension.team2.ConnectionManager;
@@ -48,4 +51,19 @@ public class CompanyDAOClass implements CompanyDAO {
 		return null;
 	}
 
+}
+
+
+// A simple prepared statement with a null fail-safe.
+public static ResultSet getDatabaseData(String[] userInput, Connection conn, PreparedStatement pstmt) {
+	try {
+		pstmt = conn.prepareStatement("SELECT first_name, last_name, credits FROM student WHERE (credits >= ? AND credits < ?) ORDER BY credits DESC;");
+		pstmt.setString(1, userInput[0]);
+		pstmt.setString(2, userInput[1]);
+		return pstmt.executeQuery();
+		
+	} catch (SQLException e) {
+		e.printStackTrace();
+		return null;
+	}
 }
