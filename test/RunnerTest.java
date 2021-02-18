2f4d6312867ea5dd83ad8d417575327e94bc4431
zelda <raymundpalafox1@gmail.com>
2021-02-18 09:38:58 -0800

    Create first test classes.

---
+++ test/RunnerTest.java
@@ -0,0 +1,14 @@
+package com.cognixia.jump.jdbc.ems_extension.team2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RunnerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
