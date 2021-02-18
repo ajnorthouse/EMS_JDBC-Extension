6249e7857f44a8e40f88c8662c792d36f1180913
Raymund <raymundpalafox1@gmail.com>
2021-02-18 09:16:04 -0800

    Updated company inserts

--- sql/inserts/companyInsert.sql
+++ sql/inserts/companyInsert.sql
@@ -1,13 +1,13 @@
 SELECT * FROM company;
 
 # comp_id: 1
-INSERT INTO company(comp_id, comp_name, comp_budget, address_id) 
-			 VALUES(null, "Tesla", "5000000", 1);
+INSERT INTO company(comp_id, comp_name, comp_budget) 
+			 VALUES(null, "Tesla", "5000000");
              
 # comp_id: 2
 INSERT INTO company(comp_id, comp_name, comp_budget, address_id)
-			 VALUES(null, "Amazon", "6500000", 2);
+			 VALUES(null, "Amazon", "6500000");
         
 # comp_id: 3
 INSERT INTO company(comp_id, comp_name, comp_budget, address_id)
-			 VALUES(null, "Apple", "6000000", 3);
+			 VALUES(null, "Apple", "6000000");
