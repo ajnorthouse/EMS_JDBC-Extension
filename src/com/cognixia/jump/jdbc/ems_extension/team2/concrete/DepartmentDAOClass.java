7b9ef4024e0fd527551a86430689291921ba72a5
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-02-18 10:35:19 -0500

    Create DAO classes.

---
+++ src/com/cognixia/jump/jdbc/ems_extension/team2/concrete/DepartmentDAOClass.java
@@ -0,0 +1,7 @@
+package com.cognixia.jump.jdbc.ems_extension.team2.concrete;
+
+import com.cognixia.jump.jdbc.ems_extension.team2.interfaces.DepartmentDAO;
+
+public class DepartmentDAOClass implements DepartmentDAO {
+
+}
---
+++ src/com/cognixia/jump/jdbc/ems_extension/team2/concrete/EmployeeDAOClass.java
@@ -0,0 +1,7 @@
+package com.cognixia.jump.jdbc.ems_extension.team2.concrete;
+
+import com.cognixia.jump.jdbc.ems_extension.team2.interfaces.EmployeeDAO;
+
+public class EmployeeDAOClass implements EmployeeDAO {
+
+}
