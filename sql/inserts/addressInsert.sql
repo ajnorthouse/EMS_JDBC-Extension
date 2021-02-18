20b984cdd8b9d896c6a0c687f85fb2d56fd4e632
Raymund <raymundpalafox1@gmail.com>
2021-02-17 21:11:28 -0800

    Completed address inserts

---
+++ sql/inserts/addressInsert.sql
@@ -0,0 +1,3 @@
+INSERT INTO address VALUES(null, "3500 Deer Creek Road", "Palo Alto", "CA", 94304);
+INSERT INTO address VALUES(null, "410 Terry Ave. North", "Seattle", "WA", 98109);
+INSERT INTO address VALUES(null, "Infinite One Loop", "Cupetino", "CA", 95014);
