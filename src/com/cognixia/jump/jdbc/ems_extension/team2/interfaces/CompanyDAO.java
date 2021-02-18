5706803547e54deb7b9a997c4ac997a76e0c5d7b
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-02-18 11:01:13 -0500

    Add classes to interface.

--- src/com/cognixia/jump/jdbc/ems_extension/team2/interfaces/CompanyDAO.java
+++ src/com/cognixia/jump/jdbc/ems_extension/team2/interfaces/CompanyDAO.java
@@ -1,5 +1,15 @@
 package com.cognixia.jump.jdbc.ems_extension.team2.interfaces;
 
-public interface CompanyDAO {
+import java.util.List;
 
+import com.cognixia.jump.jdbc.ems_extension.team2.model.Company;
+import com.cognixia.jump.jdbc.ems_extension.team2.model.Department;
+
+public interface CompanyDAO {
+	public List<Company> getAllCompanies();
+	public Company getCompany(int id);
+	public boolean createCompany(Company c);
+	public boolean deleteCompany(Company c);
+	public boolean updateCompany(Company c);
+	public List<Department> getCompanyDepartments(Company d);
 }
